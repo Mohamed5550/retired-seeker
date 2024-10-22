13aca134de9ca226974aec96d91b641a47c50f68
mohamed-rnd <mohamed.hesham@layoutinternational.com>
2024-10-22 19:49:55 +0300

    Initial commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    "users-permissions": {
+      config: {
+        register: {
+          // put the name of your added fields here
+          allowedFields: ["first_name", "last_name", "job_title", "company_name", "user_type"],
+        },
+      },
+    }
+});
