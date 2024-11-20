546fd564d27f68cfba120479c15edfe17baf5849
mohamed-rnd <mohamed.hesham@layoutinternational.com>
2024-11-21 00:03:56 +0200

    added lang header

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,7 @@ module.exports = [
     config: {
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', "lang"],
       credentials: true,
     },
   },
